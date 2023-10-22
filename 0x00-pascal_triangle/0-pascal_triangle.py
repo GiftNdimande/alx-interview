d501f1e7b856cabba969a1ed44210fc90f48b24c
GiftNdimande <sfisotsitso@gmail.com>
2023-10-22 08:48:02 +0000

    fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,7 +7,7 @@ def pascal_triangle(n):
     for i in range(n - 1):
         track = [0] + count[-1] + [0]
         row = []
-        for j in range(len(out[-1]) + 1):
+        for j in range(len(count[-1]) + 1):
             row.append(track[j] + track[j + 1])
         count.append(row)
     return count
